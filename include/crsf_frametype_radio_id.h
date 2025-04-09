a04072ce6f679c2e7047d9495a05a98e62283e43
Boris Gubanov <44787162+boris-gu@users.noreply.github.com>
2025-04-09 13:29:01 +0300

    Remove unused pkt

--- include/crsf_frametype_radio_id.h
+++ include/crsf_frametype_radio_id.h
@@ -17,7 +17,12 @@ typedef struct {
   uint8_t ext_src;   // 0xEE CRSF_ADDRESS_CRSF_TRANSMITTER
   uint8_t subtype;   // 0x10 CRSF_FRAMETYPE_OPENTX_SYNC
 
-  int32_t packet_rate; // us * 10 (Big endian)
+  // TODO: Проверить, беззнаковый ли
+  uint32_t packet_rate; // us * 10 (Big endian)
+  // TODO: Значение в десятой доле микросекунды
+  // UPD Похоже, это значение никак не используется в EdgeTX
+  // https://github.com/EdgeTX/edgetx/blob/50b32e6cfc37fe70a829e0c4b64f017993526f08/radio/src/telemetry/crossfire.cpp#L279
+  // https://github.com/EdgeTX/edgetx/blob/50b32e6cfc37fe70a829e0c4b64f017993526f08/radio/src/telemetry/telemetry.cpp#L526C64-L526C75
   int32_t shift_corr;
 } crsf_radio_id;
 
